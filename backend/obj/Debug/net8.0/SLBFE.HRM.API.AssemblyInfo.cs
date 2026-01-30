aca02ae7dc69f3802843cb32e0b08b49971c6f91
Ashan Wickramanayaka <wmashan2@gmail.com>
2026-01-30 21:33:32 +0530

    change

--- backend/obj/Debug/net8.0/SLBFE.HRM.API.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/SLBFE.HRM.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SLBFE.HRM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8133bcc53ce25e543639c3cd075d09ee6f4ce90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd60dfcc8cb08ec2aa91c6adb88b381cef90a302")]
 [assembly: System.Reflection.AssemblyProductAttribute("SLBFE.HRM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SLBFE.HRM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
