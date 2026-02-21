e1f0b1c6a880537b809177ff8e480b332c2acf5b
Ashan Wickramanayaka <wmashan2@gmail.com>
2026-02-21 21:23:38 +0530

    changes in stash 1

--- backend/obj/Debug/net8.0/SLBFE.HRM.API.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/SLBFE.HRM.API.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabec47b835d2a2a26e45796c4eeae67c569d274")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99272b3df4867997a8bd49eb3dd0841fbad97409")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99272b3df4867997a8bd49eb3dd0841fbad97409")]
 >>>>>>> Stashed changes
